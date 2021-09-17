5ef9db3856873f0c164b2c4485bdebcb11634b9d
justin <lungjyt@hva.nl>
2021-09-17 12:06:18 +0200

    Object & alphabet students made

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+import students from "./students.js"
+
+const studentContainer = document.querySelector(".squad-container")
+
+students.sort(function(a, b) {
+    let textA = a.name.toUpperCase();
+    let textB = b.name.toUpperCase();
+    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+});
+
+
+for (let i = 0; i < students.length; i++) {
+    studentContainer.innerHTML += `
+    <div id="window">
+    <div id="window-bar">
+        <p id="window-title">
+            ${students[i].name} - FDND
+        </p>
+        <div id="window-buttons">
+            <p id="exit">&#128473;</p>
+            <p id="max">&#128470;</p>
+            <p id="min">&#128469;</p>
+        </div>
+    </div>
+    <div id="window-content">
+        <img src="${students[i].foto}">
+        <a href="${students[i].link}">Visitekaart</a>
+    </div>
+</div>
+    `
+}
+
+console.log(students);
---
+++ scripts/students.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        "name": "BJustin",
+        "link": "",
+        "foto": "./assets/fdnd-vaporwave-logo.svg"
+    },
+    {
+        "name": "CJustin",
+        "link": "",
+        "foto": "./assets/fdnd-vaporwave-logo.svg"
+    },
+    {
+        "name": "Alpha man aka Daan",
+        "link": "",
+        "foto": "./assets/fdnd-vaporwave-logo.svg"
+    }
+]
