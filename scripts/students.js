f26de1deaeb8c264f9a126409646a5d8b169d3bd
DevAbdurabi <61628190+DevAbdurabi@users.noreply.github.com>
2021-09-20 12:24:09 +0200

    url added

--- scripts/students.js
+++ scripts/students.js
@@ -21,7 +21,7 @@ export default [
     },
     {
         "name": "Shauri",
-        "link": "shauri.student.fdnd.nl",
+        "link": "https://shauri.student.fdnd.nl",
         "foto": "./assets/students/shauri.JPG"
     },
     {
